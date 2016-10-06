53fc3fa5249a947f32c198c74ffa21b16ee25c16
jyapujuju <danieldangol@gmail.com>
2016-10-06 11:58:48 +0545

    filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only:  [:new, :create]
+  before_action :is_owner?, only: [:edit, :update]
   def index
     @posts = Post.all.order ('created_at DESC')
   end
@@ -36,4 +37,8 @@ class PostsController < ApplicationController
  def post_params
    params.require(:post).permit(:user_id, :photo, :description)
  end
+
+   def is_owner?
+     redirect_to root_path if Post.find(params[:id]).user != current_user
+  end
 end
