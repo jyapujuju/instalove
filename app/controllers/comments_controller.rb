2decc0f914692be59f713bc9626b9b0e44878a08
jyapujuju <danieldangol@gmail.com>
2016-10-06 15:15:12 +0545

    comments form are created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,17 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!,  only: :create
   def create
     @post = Post.find(params[:post_id])
-    @comments = current_user.comments.create(comments_params)\
+    @comments = current_user.comments.create(comments_params)
     if @comments.valid?
       redirect_to root_path
     else
       flash[:alert] = "Invalid attributes."
-      redirect_to root_paths
+      redirect_to root_path
     end
-
   end
+
   def comments_params
     params.require(:comment).permit(:text, :post_id)
   end
+
 end
