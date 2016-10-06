8f12972689e6d031383f7828d6da6552284eef90
jyapujuju <danieldangol@gmail.com>
2016-10-06 12:05:36 +0545

    adding deletion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only:  [:new, :create]
-  before_action :is_owner?, only: [:edit, :update]
+  before_action :is_owner?, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all.order ('created_at DESC')
   end
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
     else
       render :new, status: :unprocessable_entery
     end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
   end
 
  private
