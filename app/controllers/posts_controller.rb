6f31432850b620c8b98a705fdc9796c46bdb8a31
jyapujuju <danieldangol@gmail.com>
2016-10-17 17:24:22 +0545

    optimize the query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   before_action :is_owner?, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.order ('created_at DESC')
+    # @posts = Post.all.order ('created_at DESC')
+    @posts = Post.all.order('created_at DESC').includes(:user, comments: :user)
   end
 
 
